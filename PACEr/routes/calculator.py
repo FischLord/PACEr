9512701d588fcff1cf7421bfb20ba9d23c3be225
Janneck <46058672+FischLord@users.noreply.github.com>
2023-05-30 16:02:30 +0200

    rechner umbau angefangen but broke everything

--- PACEr/routes/calculator.py
+++ PACEr/routes/calculator.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request
-from helper import pace
+from helper import pace, calculatePace
 
 bp_calculator = Blueprint('calculator', __name__)
 
@@ -9,19 +9,15 @@ def pacer():
         try:
             laenge = request.form['laenge']
             laenge = int(laenge)
-            bz_sec = request.form['bz_sec']
-            hz_sec = request.form['hz_sec']
-            ez_sec = request.form['ez_sec']
-            bz_min = request.form['bz_min']
-            hz_min = request.form['hz_min']
-            ez_min = request.form['ez_min']
+            kmh = request.form['kmh']
 
             if laenge is not None:
+                calculatePace()
                 result = pace(laenge, bz_sec, hz_sec, ez_sec, bz_min, hz_min, ez_min)
-                return render_template('pacer.html', laenge=laenge, bz_sec=bz_sec, hz_sec=hz_sec, ez_sec=ez_sec, bz_min=bz_min, hz_min=hz_min, ez_min=ez_min, result=result)
+                return render_template('pacer.html', laenge=laenge, kmh=kmh, bz_sec=bz_sec, hz_sec=hz_sec, ez_sec=ez_sec, bz_min=bz_min, hz_min=hz_min, ez_min=ez_min, result=result)
 
             else:
-                return render_template('pacer.html', laenge=laenge, bz_sec=bz_sec, hz_sec=hz_sec, ez_sec=ez_sec, bz_min=bz_min, hz_min=hz_min, ez_min=ez_min, result=None)
+                return render_template('pacer.html', laenge=laenge, kmh=kmh, bz_sec=bz_sec, hz_sec=hz_sec, ez_sec=ez_sec, bz_min=bz_min, hz_min=hz_min, ez_min=ez_min, result=None)
 
         except Exception as e:
             return 'Error: ' + str(e)
