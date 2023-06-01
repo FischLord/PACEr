cfb559654f4f49f7230181d39cc041e70f92a662
FischLord <janneck3@web.de>
2023-06-01 23:03:02 +0200

    footer + layout changes + some favicons

--- PACEr/static/js/notification.js
+++ PACEr/static/js/notification.js
@@ -1,11 +1,11 @@
 // Get references to the popup and close button
-const popup = document.getElementById('popup');
-const closeBtn = document.getElementById('closeBtn');
+const popup = document.getElementById("popup");
+const closeBtn = document.getElementById("closeBtn");
 
 // Function to close the popup
 function closePopup() {
-  popup.style.display = 'none';
+    popup.style.display = "none";
 }
 
 // Add event listener to the close button
-closeBtn.addEventListener('click', closePopup);
+closeBtn.addEventListener("click", closePopup);
