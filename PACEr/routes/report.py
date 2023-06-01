edd89105962e0567f83a25b111ec272a66ab3df5
FischLord <janneck3@web.de>
2023-06-01 22:04:31 +0200

    fertigstellung  report issue

--- PACEr/routes/report.py
+++ PACEr/routes/report.py
@@ -1,4 +1,7 @@
 from flask import Blueprint, render_template, request
+from random import randint
+from datetime import date
+import os
 bp_report = Blueprint('report', __name__)
 
 
@@ -6,8 +9,31 @@ bp_report = Blueprint('report', __name__)
 def reportProblem():
     if request.method == 'POST':
         try:
-            return render_template('reportProblem.html')
+            # create folder reports/date
+            currentDate = date.today().strftime("%d.%m.%Y")
+            folderPath = 'PACEr/reports/' + currentDate
+            # when more then 100 issues get reported in one day, stop accepting reports
+            if len(os.listdir(folderPath)) <= 100:
+                if not os.path.exists(folderPath):
+                    os.makedirs(folderPath)
+
+                name = request.form['name']
+                vorname = request.form['vorname']
+                email = request.form['email']
+                issue = request.form['issue']
+                # write in text file and save in folder reports/date/name + random nuimber.txt
+                filename = name + vorname + str(randint(0, 1000)) + '.txt'
+                filepath = folderPath + '/' + filename
+                report = open(filepath, 'w')
+                report.write('Name: ' + name + '\n' + 'Vorname: ' + vorname + '\n' + 'Email: ' + email + '\n' + 'Issue: ' + issue)
+                report.close()
+                return render_template('projektInfo.html', notification='Ihr Problem wurde erfolgreich gemeldet.', notificationName='Info')
+            else:
+                return render_template('reportProblem.html', notification='Heute wurden bereits 100 Probleme gemeldet. Um eine Überlastung zu vermeiden, können heute keine weiteren Probleme gemeldet werden. Bitte versuchen Sie es morgen erneut.', notificationName='Warnung' , name=name, vorname=vorname, email=email, issue=issue)
         except Exception as e:
             return 'Error: ' + str(e)
     else:
-        render_template('reportProblem.html')
+        try:
+            return render_template('reportProblem.html')
+        except Exception as e:
+            return 'Error: ' + str(e)
