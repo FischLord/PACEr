6a186ae321084cc6e544444fcf4ff84d4a08ff9a
FischLord <janneck3@web.de>
2023-06-01 22:39:16 +0200

    impressum hinzugefügt

--- PACEr/static/js/layout.js
+++ PACEr/static/js/layout.js
@@ -13,14 +13,3 @@ window.addEventListener("resize", () => {
     document.documentElement.style.setProperty("--vh", `${vh}px`);
 });
 
-// Get references to the popup and close button
-const popup = document.getElementById('popup');
-const closeBtn = document.getElementById('closeBtn');
-
-// Function to close the popup
-function closePopup() {
-  popup.style.display = 'none';
-}
-
-// Add event listener to the close button
-closeBtn.addEventListener('click', closePopup);
